0483a8827754ee4d3508fc9b258dda77db3a3551
LordTakashi3535 <post.vielikogo@gmail.com>
2026-05-23 05:21:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -274,7 +274,6 @@ async def check_apartments(page):
             text = (await sidebar.locator("xpath=../../..").inner_text()).lower()
 
             words = [
-                "stel een vraag",
                 "bezichtiging",
                 "deelnemen",
                 "plan bezichtiging",
